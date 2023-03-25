63c96e6179a3a28ef6ddf27266574f4c5d1924b9
d1av <davi4alves@gmail.com>
2023-03-25 18:37:34 -0300

    feat: :sparkles: bind button

--- src/main/java/git/jsf/adm/PessoaBean.java
+++ src/main/java/git/jsf/adm/PessoaBean.java
@@ -4,18 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
 
+@ViewScoped
 @ManagedBean(name = "pessoaBean")
-@RequestScoped
 public class PessoaBean {
 
     private String nome;
+
+    private HtmlCommandButton commandButton;
+
+    public HtmlCommandButton getCommandButton() {
+	return commandButton;
+    }
+
+    public void setCommandButton(HtmlCommandButton commandButton) {
+	this.commandButton = commandButton;
+    }
 
     private List<String> nomes = new ArrayList<String>();
 
     public void addNome() {
 	nomes.add(nome);
+	if (nomes.size() > 3) {
+	    commandButton.setDisabled(true);
+	}
     }
 
     public List<String> getNomes() {
