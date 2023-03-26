b24fec76c71b810a93cd025dae014e5247a60f93
d1av <davi4alves@gmail.com>
2023-03-26 15:48:05 -0300

    feat: :sparkles: add table creation working

---
+++ src/main/java/git/jsf/adm/utils/HibernateUtil.java
@@ -0,0 +1,31 @@
+package git.jsf.adm.utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernateUtil {
+
+    public static EntityManagerFactory factory = null;
+
+    static {
+	init();
+    }
+
+    private static void init() {
+	try {
+
+	    if (factory == null) {
+		factory = Persistence
+			.createEntityManagerFactory("adm");
+	    }
+
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+    }
+
+    public static EntityManager getEntityManager() {
+	return factory.createEntityManager();
+    }
+}
---
+++ src/test/java/git/jsf/adm/utils/HibernateUtilTest.java
@@ -0,0 +1,12 @@
+package git.jsf.adm.utils;
+
+import org.junit.jupiter.api.Test;
+
+public class HibernateUtilTest {
+
+    @Test
+    public void testHibernateUtil() {
+	HibernateUtil.getEntityManager();
+    }
+
+}
