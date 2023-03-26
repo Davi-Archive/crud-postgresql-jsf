42ff00bb12b748bccc5143e03691bbbd5eac603c
d1av <davi4alves@gmail.com>
2023-03-26 16:42:26 -0300

    feat: :sparkles: add personalized method

--- src/test/java/git/jsf/adm/utils/HibernateUtilTest.java
+++ src/test/java/git/jsf/adm/utils/HibernateUtilTest.java
@@ -52,26 +52,26 @@ public class HibernateUtilTest {
 
 	System.out.println(pessoa);
     }
-    
+
     @Test
     public void testeUpdate() {
 	DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
-	
+
 	UsuarioPessoa pessoa = daoGeneric.pesquisarOutraForma(1L,
 		UsuarioPessoa.class);
-	
+
 	pessoa.setIdade(99);
 	pessoa.setNome("New Name");
-	
+
 	pessoa = daoGeneric.updateMerge(pessoa);
-	
+
 	System.out.println(pessoa);
     }
-    
+
     @Test
     public void testeDelete() {
 	DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
-	
+
 	UsuarioPessoa pessoa1 = daoGeneric.pesquisarOutraForma(6L,
 		UsuarioPessoa.class);
 	UsuarioPessoa pessoa2 = daoGeneric.pesquisarOutraForma(7L,
@@ -82,18 +82,31 @@ public class HibernateUtilTest {
 	daoGeneric.deletarPorId(pessoa1);
 	daoGeneric.deletarPorId(pessoa2);
 	daoGeneric.deletarPorId(pessoa3);
-	
+
 	System.out.println("Deletado com sucesso");
     }
 
-    
     @Test
     public void testeConsultar() {
-	DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric();
-	
-	List<UsuarioPessoa> list = daoGeneric.listar(UsuarioPessoa.class);
-	
-	for(UsuarioPessoa usuarioPessoa:list) {
+	DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+
+	List<UsuarioPessoa> list = daoGeneric
+		.listar(UsuarioPessoa.class);
+
+	for (UsuarioPessoa usuarioPessoa : list) {
+	    System.out.println(usuarioPessoa);
+	    System.out.println("-----------------");
+	}
+    }
+
+    @Test
+    public void testQueryList() {
+	DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+
+	List<UsuarioPessoa> list = daoGeneric.getEntityManager()
+		.createQuery(" from UsuarioPessoa where nome = 'Davi'").getResultList();
+
+	for (UsuarioPessoa usuarioPessoa : list) {
 	    System.out.println(usuarioPessoa);
 	    System.out.println("-----------------");
 	}
